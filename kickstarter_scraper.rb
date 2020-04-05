5c65b378087a2c7cee7476b40b2e383ae153e18e
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-05 20:36:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
+
+  projects = {}
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
 end
