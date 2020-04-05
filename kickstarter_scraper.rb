0fb9f8b82e8232729aba9c4162232fa1b314ad5d
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-05 20:52:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,6 +21,9 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a".text)
     projects[title.to_sym] = {
       image_link: project.css("div.project-thumbnail a img").attribute("src").value
+      description: project.css("p.bbcard_blurb").text.strip
+      location: project.css("ul.project-meta li a").text.strip
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
     }
 
