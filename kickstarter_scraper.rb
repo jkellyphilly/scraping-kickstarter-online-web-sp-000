51371948a8a127664f2a710ddf24db3c8a24db1b
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-05 20:55:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,16 +7,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-  # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text.strip (removes the new line characters)
-  # location: project.css("ul.project-meta li a").text.strip
-  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #binding.pry
 
   kickstarter.css("li.project.grid_4").each do |project|
-
     # Grab the title of the project and use it as the key to this project
     title = project.css("h2.bbcard_name strong a").text
 
